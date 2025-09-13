bcd27d828898b597e3fbfce43fa329d81845c4aa
u3648956 <u3648956@connect.hku.hk>
2025-09-13 17:49:47 +0800

    Update spongbob.py

--- spongbob.py
+++ spongbob.py
@@ -6,7 +6,7 @@ screen.bgcolor("white")
 
 # Create turtle
 sponge = turtle.Turtle()
-sponge.speed(5)
+sponge.speed(10)
 sponge.pensize(3)
 
 # Draw the main body (a rectangle)
@@ -34,7 +34,7 @@ def draw_eye(x, y):
     sponge.end_fill()
     # Pupil
     sponge.penup()
-    sponge.goto(x, y + 10)
+    sponge.goto(x, y + 20)
     sponge.pendown()
     sponge.color("black")
     sponge.begin_fill()
@@ -50,6 +50,36 @@ def draw_mouth():
     sponge.color("black")
     sponge.width(4)
     sponge.circle(50, 120)
+
+# Draw teeth
+def draw_teeth1():
+    sponge.penup()
+    sponge.goto(0, -70)
+    sponge.setheading(0)
+    sponge.pendown()
+    sponge.color("white")
+    sponge.begin_fill()
+    for _ in range(2):
+        sponge.forward(10)
+        sponge.left(90)
+        sponge.forward(20)
+        sponge.left(90)
+    sponge.end_fill()
+
+def draw_teeth2():
+    sponge.penup()
+    sponge.goto(-15, -70)
+    sponge.setheading(0)
+    sponge.pendown()
+    sponge.color("white")
+    sponge.begin_fill()
+    for _ in range(2):
+        sponge.forward(10)
+        sponge.left(90)
+        sponge.forward(20)
+        sponge.left(90)
+    sponge.end_fill()
+
 
 # Draw body2
 def draw_body2():
@@ -82,6 +112,25 @@ def draw_legs():
     sponge.setheading(-90)
     sponge.forward(50)
 
+#Draw arms
+def draw_arm1():
+    sponge.penup()
+    sponge.goto(-100, 0)
+    sponge.setheading(180)
+    sponge.pendown()
+    sponge.color("yellow")
+    sponge.width(10)
+    sponge.circle(-40, 120)
+
+def draw_arm2():
+    sponge.penup()
+    sponge.goto(100, 0)
+    sponge.setheading(0)
+    sponge.pendown()
+    sponge.color("yellow")
+    sponge.width(10)
+    sponge.circle(40, 120)
+
 # Draw the body
 draw_body1()
 draw_body2()
@@ -92,9 +141,13 @@ draw_eye(50, 30)
 
 # Draw mouth
 draw_mouth()
+draw_teeth1()
+draw_teeth2()
 
 # Draw legs
 draw_legs()
+draw_arm1()
+draw_arm2()
 
 # Finish drawing
 sponge.hideturtle()
